298c6f0e24868f9af09c11505910704bc3ddc001
ozturk sirin <ozturksirininfo@gmail.com>
2024-05-20 20:53:37 +0300

    chore: Update component and screen exports
    This commit updates the exports in the `Components` and `Screens` directories. In `Components/index.ts`, the default export is changed to a named export. In `Screens/index.ts`, a new import statement for the `Home` component is added and exported. This change ensures that the components and screens can be imported correctly in other parts of the codebase.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Router from "@/Router/Router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Router />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 8,
   },
 });
---
+++ src/Screens/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { HomeProps } from "./Index";
+import { Text, View } from "react-native";
+
+const Home = (props: HomeProps) => {
+  return (
+    <>
+      <View>
+        <Text>HOME SCREENS</Text>
+      </View>
+    </>
+  );
+};
+
+export default Home;
