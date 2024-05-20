da380b4f8418d8b37fd74ec28cf7c40366c6e7ed
ozturk sirin <ozturksirininfo@gmail.com>
2024-05-20 19:22:05 +0300

    chore: Update tsconfig.json with additional compiler options

---
+++ src/Router/Router.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { RouterProps } from "./Index";
+import { View } from "react-native";
+
+const Router = (props: RouterProps) => {
+  return (
+    <>
+      <View></View>
+    </>
+  );
+};
+
+export default Router;
